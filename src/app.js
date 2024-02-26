07367e1639c745afa07af525c6e1a49a0326977e
AlanMezva <L20170749@culiacan.tecnm.mx>
2024-02-25 19:06:59 -0700

    Fixing bugs on package.json

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 import express from "express";
 import morgan from "morgan";
 
-import employeesRoutes from "./routes/employees.routes.js";
 import indexRoutes from "./routes/index.routes.js";
 
 const app = express();
+
+import {PORT} from "./config.js";
+
+app.listen(PORT);
+console.log(`Server on port http://localhost:${PORT}`);
 
 // Middlewares
 app.use(morgan("dev"));
@@ -12,7 +16,6 @@ app.use(express.json());
 
 // Routes
 app.use("/", indexRoutes);
-app.use("/api", employeesRoutes);
 
 app.use((req, res, next) => {
 	res.status(404).json({message: "Not found"});
