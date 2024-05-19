451501ddbf333b4d0f43ee0816531cb815f0bc82
AlanMezva <L20170749@culiacan.tecnm.mx>
2024-05-19 13:22:34 -0600

    FIx bug on dishes CRUD

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(cors());
 // Routes
 app.use("/", indexRoutes);
 app.use("/api", employeesRoutes);
-app.use("api", dishesRoutes);
+app.use("/api", dishesRoutes);
 
 app.use((req, res, next) => {
 	res.status(404).json({message: "Not found"});
